1e47daddf583feadf38eee804d3ff790e7dbc6ea
Kcepriu <kcepriu@gmail.com>
2023-10-05 21:19:55 +0300

    change video player

--- src/components/ProductAddInfoVideos/ProductAddInfoVideos.tsx
+++ src/components/ProductAddInfoVideos/ProductAddInfoVideos.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { FC } from "react";
-import YouTube, { YouTubeProps } from "react-youtube";
+// import YouTube, { YouTubeProps } from "react-youtube";
 import style from "./ProductAddInfoVideos.module.css";
 
 interface IProps {
@@ -14,31 +14,42 @@ const getVideoId = (url: string): string => {
 };
 
 const ProductAddInfoVideos: FC<IProps> = ({ videos }) => {
-  const opts = {
-    height: "460",
-    width: "818",
-  };
+  // const opts = {
+  //   height: "460",
+  //   width: "818",
+  // };
 
-  const onPlayerReady: YouTubeProps["onReady"] = (event) => {
-    // access to player in all event handlers via event.target
-    event.target.pauseVideo();
-  };
+  // const onPlayerReady: YouTubeProps["onReady"] = (event) => {
+  //   // access to player in all event handlers via event.target
+  //   event.target.pauseVideo();
+  // };
 
   return (
     <div className={style.wrapContent}>
       {videos.map((video, index) => {
         return (
-          <YouTube
-            key={index}
-            videoId={getVideoId(video.url)}
-            // videoId="SUrLrFYFo6Q"
-            opts={opts}
-            onReady={onPlayerReady}
-          />
+          <div key={index}>
+            <iframe
+              width="853"
+              height="480"
+              src={`https://www.youtube.com/embed/${getVideoId(video.url)}`}
+              // frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              title="Embedded youtube"
+            />
+          </div>
         );
       })}
     </div>
   );
 };
 
+// <YouTube
+//   key={index}
+//   videoId={getVideoId(video.url)}
+//   // videoId="SUrLrFYFo6Q"
+//   opts={opts}
+//   onReady={onPlayerReady}
+// />
 export default ProductAddInfoVideos;
